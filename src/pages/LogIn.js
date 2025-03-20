56fbb04d6fcf9ae59a7cd5297728b43d8bdf96de
ntombozukondamase47 <ntombozukondamase47@gmail.com>
2025-03-20 23:59:04 +0200

    Updated Contact page and added login and sign up

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,12 +1,47 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "../styles/LogIn.css";
+import { Link } from "react-router-dom";
 
 function LogIn() {
+  return (
+    <Container className="main-content">
+      <Row className="justify-content-center">
+        <Col md={6} className="left-side border p-3">
+          <h3>Welcome Back!</h3>
+          <p>
+            Not a member yet?{" "}
+            <b>
+              <Link to="/SignUp">Register now</Link>
+            </b>
+          </p>
+        </Col>
+        <Col md={6} className="right-side border p-3">
+          <h4>Please login to your account</h4>
 
-    return(
-
-        <div></div>
-
-    );
-
+          <Form className="form-container">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control type="email" placeholder="Email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <div className="d-grid gap-2">
+              <Button className="btn btn-secondary btn-lg" type="submit">
+                Log In
+              </Button>
+            </div>
+            <p>Forgot your password?</p>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
+
 export default LogIn;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,12 +1,66 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "../styles/SignUp.css";
+import { Link } from "react-router-dom";
 
 function SignUp() {
+  return (
+    <Container className="main-content">
+      <Row className="justify-content-center">
+        <Col md={6} className="left-side border p-3">
+          <h3>Welcome!</h3>
+          <p>
+            Are you a member?{" "}
+            <b>
+              <Link to="/LogIn">Log in now</Link>
+            </b>
+          </p>
+        </Col>
 
-    return(
+        <Col md={6} className="right-side border p-3">
+          <h4>Register with your email</h4>
 
-        <div></div>
+          <Form className="form-container">
+            <Row>
+              <Col md={6}>
+                <Form.Group className="mb-3" controlId="formFirstName">
+                  <Form.Control type="text" placeholder="First Name" />
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group className="mb-3" controlId="formLastName">
+                  <Form.Control type="text" placeholder="Last Name" />
+                </Form.Group>
+              </Col>
+            </Row>
 
-    );
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control type="email" placeholder="Email" />
+            </Form.Group>
 
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formConfirmPassword">
+              <Form.Control type="password" placeholder="Repeat password" />
+            </Form.Group>
+
+            <div className="d-grid gap-2">
+              <Button className="btn btn-secondary btn-lg" type="submit">
+                Sign Up
+              </Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
+
 export default SignUp;
